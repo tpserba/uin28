7f8ce23394ce4d3e0e9d167740d5c79536f9bf90
Tomás Praga <tpraga@serbatic.es>
2023-05-03 16:35:39 +0200

    first commit

---
+++ src/test/java/com/in28minutes/serbatic/tpraga/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.serbatic.tpraga.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
