2afd35e332ef7403376da5c45297ad2f8764c2ac
Tomás Praga <tpraga@serbatic.es>
2023-05-08 16:52:39 +0200

    implement delete todo

--- src/main/java/com/in28minutes/serbatic/tpraga/todo/TodoService.java
+++ src/main/java/com/in28minutes/serbatic/tpraga/todo/TodoService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 @Service
 public class TodoService {
@@ -27,5 +28,10 @@ public class TodoService {
     public void addTodo(String username, String description, LocalDate targetDate, boolean done){
         Todo todo = new Todo(++todosCount, username, description, targetDate, done);
         todos.add(todo);
+    }
+
+    public void deleteById(int id){
+        Predicate<? super Todo> predicate = todo ->todo.getId() == id;
+        todos.removeIf(predicate);
     }
 }
